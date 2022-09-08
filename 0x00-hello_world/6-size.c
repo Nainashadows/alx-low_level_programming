d32f56d90524e9bebd5d7b55248db1e2d902e68e
Nainashadows <nainashadows@gmail.com>
2022-09-08 16:44:07 -0700

    6th Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,21 +3,23 @@
 /**
  * main - Entry Point
  *
+ * Description: using the main function
+ * this program prints the value of data types
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int a;
-	long int b;
-	long long int c;
+	long  b;
+	long long  c;
 	char d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of(c))
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(f));
+	printf("Size of a char: %lu byte(s)\n", size of(d));
+	printf("Size of an int: %lu byte(s)\n", size of(a));
+	printf("Size of a long int: %lu byte(s)\n", size of(b));
+	printf("Size of a long long int: %lu byte(s)\n", size of(c));
+	printf("Size of a float: %lu byte(s)\n", size of(f));
 	return (0);
 }
