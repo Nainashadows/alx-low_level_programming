6b5b22df99da75fa6927b3cd82ff9f23151e4516
Nainashadows <nainashadows@gmail.com>
2022-09-09 12:46:25 -0700

    Tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,22 +2,21 @@
 /**
  * main - print if the number is positive, negative or zero
  *
- * Description: using the main function 
- * this program prints a set of values
+ * Description: using the main function
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'Z' ; ch++)
-	{	
-		putchar(ch);
-	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	{
-		
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+char ch;
+
+for (ch = 'a' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
