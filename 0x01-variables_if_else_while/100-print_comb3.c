c17ec4e95136870beb20ca83daa21a6aafc45328
Nainashadows <nainashadows@gmail.com>
2022-09-09 15:43:14 -0700

    10th Task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print a set of numbers
+ *
+ * Description: use the main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1 = o, n2;
+
+	while (n1 <= 9)
+	{
+		n2 = 0;
+		while (n2 <= 9)
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				putchar(n1 + 48);
+				putchar(n2 + 48);
+
+				if (n1 + n2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('\n');
+	return (0);
+}
