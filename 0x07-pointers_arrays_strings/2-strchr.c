c0b46100c7bb0aeae083517e001228807b8bf858
Nainashadows <nainashadows@gmail.com>
2022-09-26 10:30:33 -0700

    2nd Task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,18 +12,13 @@
 
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
-		{
-			a = *s++;
-		}
-		if (a == c)
-		{
-		return (s - 1);
-		}
-		if (a == 0)
-		{
-		return (NULL);
-		}
+	while (*s != 0)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (0);
 }
