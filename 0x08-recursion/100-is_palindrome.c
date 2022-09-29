391c8e22c4d06dcb4370ef92c38c126d5e63e0dd
Nainashadows <nainashadows@gmail.com>
2022-09-29 11:52:06 -0700

    Advanced Tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,9 +27,9 @@ int is_palindrome(char *s)
 
 int _strlen_recursion(char *s)
 {
-		if (*s == '\0')
-					return (0);
-			return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
