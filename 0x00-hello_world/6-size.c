d2ae00480dab0c03832a55c571f3ddd3776fefd2
Nainashadows <nainashadows@gmail.com>
2023-08-10 14:34:52 -0700

    Mandatory Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
-	printf("Size of a float: %zu byte(s)\n", sizeof(f);
-
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
