a6ded81bca71a4d2ecfbe3c16305d26b34ef910f
Nainashadows <nainashadows@gmail.com>
2023-08-10 13:17:10 -0700

    Mandatory Tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ * this program prints "programming is  like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(Mystr);
+
+	return (0);
+}
